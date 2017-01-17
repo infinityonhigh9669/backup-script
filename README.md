03a183f751f3543db49121f3b44d06c741db4c04
T-Bone Lu <infinityonhigh9669@gmail.com>
2017-01-17 15:12:44 +0800

    Initial Version.

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-# backup-script
+Centos-Backup-Script
+====================
+
+The purpose of this script is to create daily backup archives (`tar.gz`) of selected directories/files and/or MYSQL dumps.
+
+> NOTE: This script is intended for expert users, 
+please read carefully the script and the instructions and use it at your own risk.
+
+This script support:
+
+* Backup selected directories & files.
+* Backup selected MySQL Databases dump.
+* Compress and archive all into one `.tar.gz` file.
+* Create .tgz in a backup folder for each backup session.
+* Delete older backups and keep only the latest backups (6 by default).
+* Save each backup archive in a different directory (/backup/0 .. backup/6).
+
+> root access may be required to run this script.
+
+Documentation
+-------------
+
+### Installation
+
+1. Clone this script from github:
+
+	    mkdir /scripts
+	    cd /scripts
+	    git clone https://github.com/infinityonhigh9669/backup-script
+
+	or copy the files manually to your prefered directory.
+
+2. Create setting.cfg from the included settings.cfg.example.
+
+	    cp settings.cfg.example settings.cfg
+
+	and update your mysql user/password inside.
+	update also the `workdir` folder to the location of this script
+
+### Configure backups
+Create file inside the `/lst` folder called `folders.lst`.
+The script reads for directories/files to backup. Input list of all folders to backup in one row:
+
+		/var/log/ /var/www/ /usr/files/ /tftpd/
+
+### How backup files are saved
+all backups are saved by default inside your `workdir`/backup folder. each backup inside a seperate subdirectory.
+
+* by default, the script saves 6 older backups. this can be changed using the `BACKUP_COPIES` settings (inside settings.cfg).
+* first backup will be saved at /$workdir/backup/0
+* at second backup, 0 folder will renamed 1 and the new backup will be saved at /$workdir/backup/0 again.
+* when all 7 folders (6 older and the current - /backup/0) are full - folder /backup/6 will be deleted; 5 will be renamed to 6; 4 to 5.. etc.. 0 again will be the newest backup.
+
+> backup/0 is the newer backup. backup/6 is the oldest backup  
+
+you can set the backup destination to another folder by manually editing the `backupdir` setting. 
+
+### Enable script
+By default, the script is set in test-mode (NO BACKUP). 
+
+1. Configure the settings, Test the script in your environment in testmode and verify using the console output that everything is ok. disable testmode by changing the setting inside your settings.cfg:
+ 
+		WRITE_CHANGES=true
+
+### Setup cron
+Setup cron to run your script daily (or at any timing you want).
+
+for example:
+
+	    crontab -e
+
+add inside:
+
+	    0 0 * * * /scripts/Centos-Backup-Script/backup.sh
---
+++ backup.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+# --------------------------------------------------------
+# Centos-Server-Backup-Script.
+#
+#
+# by T-Bone Lu 
+# https://tbonelu.info
+# GitHub: https://github.com/infinityonhigh9669/backup-script
+#
+# this script backup important files from your system:
+# system files, db files, user files, services, etc.,
+# and create .tgz in a backup folder for each backup session.
+# root access required.
+# --------------------------------------------------------
+
+function checkLists {
+    if ! [ -f $backuplistfile ] ; then
+        echo "Missing backup Listfile. create $backuplistfile"
+        exit
+    fi;
+}
+
+function checkBackupStatus {
+    if $BACKUP_DAILY_ONLY_ONCE ; then
+        if [ -f $backupdir/0/$filename ] ; then
+            echo "Backup already exist - try again tomorrow."
+            exit;
+        fi;
+    fi;
+    if [ -d $tempdir ] ; then
+        echo ""
+        echo "Backup is already running. remove temp folder to reset."
+        exit;
+    fi;
+}
+
+function createTemporaryFolder {
+    printf "Creating temporary directory.. "
+    if $WRITE_CHANGES ; then
+        mkdir $tempdir
+        printf "Ok\n"
+    else printf "Skipping\n"
+    fi;
+}
+
+function deleteOldestBackup {
+    if $WRITE_CHANGES ; then
+        if [ -d $backupdir/$1/ ] ;
+        then
+            echo "Deleteing Number $1";
+            rm -r -f $backupdir/$1/ ;
+        fi;
+    fi;
+}
+
+function shiftBackup {
+    if [ -d $backupdir/$1/ ] ; then
+        printf "Moving Number $1 to Number $2.. ";
+        if $WRITE_CHANGES ; then
+            mv $backupdir/$1 $backupdir/$2/ ;
+            printf "Ok\n"
+        else printf "Skipping\n"
+        fi;
+    fi;
+}
+
+function shiftBackups {
+    for (( c=$BACKUP_COPIES; c>0; c-- ))
+    do
+        b=$c
+        let "b -= 1"
+        shiftBackup $b $c
+    done
+}
+
+function dumpSQL {
+    printf "Regenerating DB list file.. ";
+    if $WRITE_CHANGES ; then
+        mysql -u $SQL_USER -p$SQL_PASSWD -Bse 'show databases' > $listfile
+        printf "Dumping SQL Databases.. ";
+        cat $listfile | while read line
+        do
+            dbname=$line
+            if [ $line != "information_schema" ] ;
+            then
+                mysqldump --events --ignore-table=mysql.events -u $SQL_USER -p$SQL_PASSWD $dbname > $tempdir/$dbname.sql
+            fi
+        done
+        printf "Ok\n"
+    else printf "Skipping\n"
+    fi;
+}
+
+function createBackup {
+    echo "Creating TGZ Backup file for..";
+    echo "directories:"
+    cat $backuplistfile | while read line
+    do
+        for d in $line; do
+    	    echo $d
+    	    # take target directory to backup and replace / with _ for backup filename
+    	    target_backup_file=$tempdir/${d//[\/]/_}$filename
+    	    if $WRITE_CHANGES ; then
+        	tar zcfP $target_backup_file $d > $workdir/log/$filename
+    	    fi;
+            done
+            break
+        done
+    echo "databases"
+    if $WRITE_CHANGES ; then
+        tar zcfP $tempdir/db.$filename $tempdir/*.sql > $workdir/log/db.$filename
+    fi;
+}
+
+function moveBackup {
+    printf "Move from temp to Backup Number 0.. ";
+    if $WRITE_CHANGES ; then
+        mkdir $backupdir/0/
+        #mv $tempdir/$filename $backupdir/0/ ;
+        mv $tempdir/*$filename $backupdir/0/ ;
+        printf "Ok\n"
+    else printf "Skipping\n"
+    fi;
+}
+
+function cleanBackup {
+    printf "Cleaning.. ";
+    if $WRITE_CHANGES ; then
+        rm -r -f $tempdir/
+        printf "Ok\n"
+    else printf "Skipping\n"
+    fi;
+}
+
+function startBackup {
+    if $DISABLED ; then
+        echo "Skipping backup - script disabled"
+        exit
+    else
+        checkLists
+        checkBackupStatus
+        if $WRITE_CHANGES ; then
+            echo "Starting Backup..."
+        else
+            echo "Running in test mode..."
+        fi;
+        createTemporaryFolder
+        # step 1: delete oldest backup
+        deleteOldestBackup $BACKUP_COPIES
+        # step 2: shift the middle snapshots(s) back by one, if they exist
+        shiftBackups
+        # step 3: dump sql dbs
+        dumpSQL
+        # step 4: create new backup
+        createBackup
+        # step 5: move to location 0
+        moveBackup
+        # step 6: clear temp for the next run
+        cleanBackup
+    fi;
+}
+
+# Intro
+echo "Edited by TBoneLu"
+# Load settings
+SCRIPTDIRECTORY=$(cd `dirname $0` && pwd)
+cd $SCRIPTDIRECTORY
+if [ -f settings.cfg ] ; then
+    echo "Loading settings..."
+    source settings.cfg
+else
+    echo "ERROR: Create settings.cfg (from settings.cfg.example)"
+    exit
+fi;
+# Start backup
+startBackup
+# Final
+echo
+if $showfsz ; then
+    df -h
+fi;
+echo "All done"
